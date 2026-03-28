969c0b7254ca44de07d4dc104bf35f10d6b4ec6c
Gustavo Carvalho <gustavoaquinocar@gmail.com>
2026-03-27 21:00:31 -0300

    adding logic

--- scripts/send_messages/send_resultados.py
+++ scripts/send_messages/send_resultados.py
@@ -6,12 +6,10 @@ SHEET_ID                   = os.environ['SHEET_ID']
 gc                         = ag.autenticar_sheets()
 EVOLUTION_PHONE_RESULTADOS = os.environ['EVOLUTION_PHONE_RESULTADOS']
 
-# resultados          = ag.ler_planiha_df(gc, 'resultados', SHEET_ID)
-# times               = ag.ler_planiha_df(gc, 'times', SHEET_ID)
-# jogos               = ag.ler_planiha_df(gc, 'jogos', SHEET_ID)
-# mensagem_resultados = aw.montar_mensagem_resultados(resultados, jogos, times)
+resultados          = ag.ler_planiha_df(gc, 'resultados', SHEET_ID)
+times               = ag.ler_planiha_df(gc, 'times', SHEET_ID)
+jogos               = ag.ler_planiha_df(gc, 'jogos', SHEET_ID)
+mensagem_resultados = aw.montar_mensagem_resultados(resultados, jogos, times)
 
-# if mensagem_resultados is not None:
-    # aw.enviar_whatsapp(mensagem_resultados, EVOLUTION_PHONE_RESULTADOS)
-
-aw.enviar_whatsapp('seu ze mane', '5519971656937')
+if mensagem_resultados is not None:
+    aw.enviar_whatsapp(mensagem_resultados, EVOLUTION_PHONE_RESULTADOS)
